92a296b992f06b63149180c0ca6fbdf76fd4cf01
zi-eunn <jieunkim1117@gmail.com>
2026-03-16 14:09:41 +0900

    refactor: Option.jsx, Option.module.css 파일 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ import Home from "./pages/Customer/Home.jsx";
 import Login from './pages/Customer/Login.jsx';
 import Register from './pages/Customer/Register.jsx';
 import MyPage from './pages/Customer/MyPage.jsx';
-import Option from './pages/Customer/Option.jsx';
-// import FastTask from './pages/Customer/FastTask.jsx';
-// import ConsultTask from './pages/Customer/ConsultTask.jsx';
-// import SpecialTask from './pages/Customer/SpecialTask.jsx';
+// import Option from './pages/Customer/Option.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import AdminLogin from './pages/Admin/AdminLogin.jsx';
 import AdminMain from './pages/Admin/AdminMain.jsx';
@@ -27,11 +24,8 @@ function App() {
         <Route path="/Register" element={<Register />} />
         
         {/* Customer 전용 페이지 */}
-        <Route path="/Option" element={<PrivateRoute allowedRoles={['customer']}><Option /></PrivateRoute>} />
+        {/* <Route path="/Option" element={<PrivateRoute allowedRoles={['customer']}><Option /></PrivateRoute>} /> */}
         <Route path="/My" element={<PrivateRoute allowedRoles={['customer']}><MyPage /></PrivateRoute>} />
-        {/* <Route path="/fast-task" element={<PrivateRoute allowedRoles={['customer']}><FastTask /></PrivateRoute>} />
-        <Route path="/consult-task" element={<PrivateRoute allowedRoles={['customer']}><ConsultTask /></PrivateRoute>} />
-        <Route path="/special-task" element={<PrivateRoute allowedRoles={['customer']}><SpecialTask /></PrivateRoute>} /> */}
         
         <Route path="/AdminLogin" element={<AdminLogin />} />
         
