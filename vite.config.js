3b6d980baacf1527704d1025abad7c455a7553f1
parkgyumin <krumin@naver.com>
2026-05-02 03:07:20 +0900

    -feat ( vite.config.js ) : 로컬호스트 프록시(CORS) 설정 - 8000포트 연결 ( fast-api 서버 ) - 기존과 스프링 파이썬 서버를 로컬호스트에서 동시 실행 후 /py로 시작하는 엔드포인트로 구성된 파이썬 서버로 비동기 통신을 진행하면됨

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,25 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-/*    host: true,
-    port: 5173,*/
     proxy: {
+      // 1. 기존 Spring Boot (Java) 서버 연결
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
+      // 2. 신규 FastAPI (Python/AI) 서버 연결
+      '/py': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false
+      },
     },
   },
 })
 
-
+/*    host: true,
+    port: 5173,*/
 /*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
